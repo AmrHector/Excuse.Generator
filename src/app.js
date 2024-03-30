e97a65a81fc0f6efd3f3b6bbdc9a157c5f9b2689
Hector Ector <163536279+AmrHector@users.noreply.github.com>
2024-03-30 20:24:04 +0000

    Pending changes exported from your codespace

--- src/app.js
+++ src/app.js
@@ -1,11 +1,45 @@
-/* eslint-disable */
-import "bootstrap";
-import "./style.css";
+window.onload = () => {
+  // Function to generate a random excuse
+  let generateExcuse = () => {
+    let who = ["The dog", "My grandma", "The mailman", "My bird"];
+    let action = ["ate", "peed", "crushed", "broke"];
+    let what = ["my homework", "my phone", "the car"];
+    let when = [
+      "before the class",
+      "when I was sleeping",
+      "while I was exercising",
+      "during my lunch",
+      "while I was praying"
+    ];
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+    let subjectIndex = Math.floor(Math.random() * who.length);
+    let actionIndex = Math.floor(Math.random() * action.length);
+    let whereIndex = Math.floor(Math.random() * what.length);
+    let posIndex = Math.floor(Math.random() * when.length);
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+    return (
+      who[subjectIndex] +
+      " " +
+      action[actionIndex] +
+      " " +
+      what[whereIndex] +
+      " " +
+      when[posIndex] +
+      "."
+    );
+  };
+
+  // Function to update the HTML with the generated excuse
+  let generateExcuseAndUpdateHTML = () => {
+    let excuse = generateExcuse();
+    document.getElementById("the-excuse").innerHTML = excuse;
+  };
+
+  // Button event listener to generate a new excuse
+  document
+    .getElementById("generate-excuse-btn")
+    .addEventListener("click", generateExcuseAndUpdateHTML);
+
+  // Initial generation of excuse
+  generateExcuseAndUpdateHTML();
 };
